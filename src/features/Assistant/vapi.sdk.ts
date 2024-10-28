bae8e992ecc6e0dccc1da292b6c75a3b99e330e5
dragutin-oreski <dragutin.oreski@gmail.com>
2024-10-28 02:44:43 +0100

    fix: add debug

--- src/features/Assistant/vapi.sdk.ts
+++ src/features/Assistant/vapi.sdk.ts
@@ -1,4 +1,5 @@
 import Vapi from "@vapi-ai/web";
 import { envConfig } from "../../config/env.config";
+console.log("config", envConfig.vapi.token);
 
 export const vapi = new Vapi(envConfig.vapi.token);
